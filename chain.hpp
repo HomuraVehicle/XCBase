c9e176f5cd244d51043fe31d8278390fb8a15e04
hmito <hmito@outlook.com>
2016-12-08 23:57:36 +0000

    Change chain for base class version.

--- chain.hpp
+++ chain.hpp
@@ -5,40 +5,100 @@
 #include<functional>
 #include"rvalue_reference.hpp"
 namespace xc{
+	struct chain_base;
+	struct chain_element{
+		friend struct chain_base;
+	private:
+		chain_element* prev;
+		chain_element* next;
+	public:
+		chain_element() :prev(0), next(0){}
+	};
+	struct chain_base{
+		typedef chain_element element;
+		static void clear_element(element& Elem){
+			Elem.next = 0;
+			Elem.prev = 0;
+		}
+		static bool insert_element(element& New, element& Pos){
+			if(New.next != 0 || New.prev != 0)return true;
+
+			Pos.prev->next = &New;
+			New.prev = Pos.prev;
+
+			Pos.prev = &New;
+			New.next = &Pos;
+
+			return false;
+		}
+		static bool splice_range_element(element& First, element& Last, element& Pos){
+			if(First.next != 0 || First.prev != 0)return true;
+			if(Last.next != 0 || Last.prev != 0)return true;
+
+			element& Before = *(Pos.prev);
+
+			Last.prev->next = &Pos;
+			First.prev->next = &Last;
+
+			Pos.prev = Last.prev;
+			Last.prev = First.prev;
+
+			First.prev = &Before;
+			Before.next = &First;
+
+			return false;
+		}
+		static bool splice_element(element& Elem, element& Pos){
+			if(Elem.next != 0 || Elem.prev != 0)return true;
+
+			element& Before = *(Pos.prev);
+
+			Elem.next->prev = Elem.prev;
+			Elem.prev->next = Elem.next;
+
+			Before.next = &Elem;
+			Elem.prev = &Before;
+
+			Before.prev = &Elem;
+			Elem.next = &Pos;
+
+			return false;
+		}
+		static bool erase_element(element& Erased){
+			if(Erased.next == 0 || Erased.prev == 0)return true;
+
+			Erased.next->prev = Erased.prev;
+			Erased.prev->next = Erased.next;
+
+			clear_element(Erased);
+
+			return false;
+		}
+		static unsigned int erase_range_element(element& First, element& Last){
+			if(First.next == 0 || First.prev == 0)return 0;
+			if(Last.next == 0 || Last.prev == 0)return 0;
+
+			element* Ptr = &First;
+			First.prev->next = &Last;
+			Last.prev = First.prev;
+
+			unsigned int Cnt = 0;
+			while(Ptr != &Last){
+				element* Cur = Ptr;
+				Ptr = Ptr->next;
+				clear_element(*Cur);
+				++Cnt;
+			}
+
+			return Cnt;
+		}
+	};
 	template<typename T>
-	struct chain{
+	struct chain: private chain_base{
 	public:
 		typedef chain<T> this_type;
 		typedef unsigned int size_type;
-	public:
-		struct element{
-			friend struct chain<T>;
-		private:
-			T value;
-			element *prev;
-			element *next;
-		public:
-			element(T value_ = T()) :value(value_), prev(0), next(0){}
-			element(xc::rvalue_reference<T> rRef_) :value(rRef_), prev(0), next(0){}
-			element(const element& Elm) :value(Elm.value), prev(0), next(0){}
-			element& operator=(const element& Elm){
-				if(this != &Elm){
-					value = Elm.value;
-				}
-				return *this;
-			}
-		public:
-			operator bool()const{ return next == 0; }
-			T& operator*(){ return value; }
-			const T& operator*()const{ return value; }
-			T* operator->(){ return &value; }
-			const T* operator->()const{ return &value; }
-		public:
-			static void connect(element& Prev, element& Next){
-				Prev.next = &Next;
-				Next.prev = &Prev;
-			}
-		};
+		typedef chain_base base;
 	public:
 		struct const_iterator;
 		struct iterator :public std::iterator<std::bidirectional_iterator_tag, T>{
@@ -73,10 +133,10 @@ namespace xc{
 				this->operator--();
 				return Ans;
 			}
-			T& operator*(){ return Cur->operator*(); }
-			const T& operator*()const{ return Cur->operator*(); }
-			T* operator->(){ return Cur->operator->(); }
-			const T* operator->()const{ return Cur->operator->(); }
+			T& operator*(){ return *static_cast<T*>(Cur); }
+			const T& operator*()const{ return *static_cast<const T*>(Cur); }
+			T* operator->(){ return static_cast<T*>(Cur); }
+			const T* operator->()const{ return static_cast<const T*>(Cur); }
 			operator const_iterator();
 			friend bool operator==(const iterator& Itr1, const iterator& Itr2){
 				return Itr1.Cur == Itr2.Cur;
@@ -89,10 +149,10 @@ namespace xc{
 		};
 		struct const_iterator :public std::iterator<std::bidirectional_iterator_tag, const T>{
 		private:
-			element* Cur;
+			const element* Cur;
 		public:
 			const_iterator() :Cur(0){}
-			explicit const_iterator(element* Cur_) :Cur(Cur_){}
+			explicit const_iterator(const element* Cur_) :Cur(Cur_){}
 			const_iterator(const iterator& Other) :Cur(Other.current()){}
 			const_iterator(const const_iterator& Other) :Cur(Other.Cur){}
 			const_iterator& operator=(const const_iterator& Other){
@@ -120,8 +180,8 @@ namespace xc{
 				this->operator--();
 				return Ans;
 			}
-			const T& operator*()const{ return Cur->operator*(); }
-			const T* operator->()const{ return Cur->operator->(); }
+			const T& operator*()const{ return *static_cast<const T*>(Cur); }
+			const T* operator->()const{ return static_cast<const T*>(Cur); }
 			friend bool operator==(const const_iterator& Itr1, const const_iterator& Itr2){
 				return Itr1.Cur == Itr2.Cur;
 			}
@@ -129,7 +189,7 @@ namespace xc{
 				return Itr1.Cur != Itr2.Cur;
 			}
 		public:
-			element* current(){ return Cur; }
+			const element* current()const{ return Cur; }
 		};
 	private:
 		element Sentinel;
@@ -147,26 +207,18 @@ namespace xc{
 				return;
 			}
 
-			element::connect(Sentinel, *(rref.ref.Sentinel.next));
-			element::connect(*(rref.ref.Sentinel.prev), Sentinel);
 			Size = rref.ref.Size;
-
-			rref.ref.Sentinel.next = &(rref.ref.Sentinel);
-			rref.ref.Sentinel.prev = &(rref.ref.Sentinel);
 			rref.ref.Size = 0;
+			base::splice_range_element(*(rref.ref.Sentinel.next), rref.ref.Sentinel,Sentinel);
 		}
 		this_type& operator=(rvalue_reference<this_type>& rref){
 			if(this!=&(rref.ref)){
 				clear();
 
 				if(!rref.ref.empty()){
-					element::connect(Sentinel, *(rref.ref.Sentinel.next));
-					element::connect(*(rref.ref.Sentinel.prev), Sentinel);
 					Size = rref.ref.Size;
-
-					rref.ref.Sentinel.next = &(rref.ref.Sentinel);
-					rref.ref.Sentinel.prev = &(rref.ref.Sentinel);
 					rref.ref.Size = 0;
+					base::splice_range_element(*(rref.ref.Sentinel.next), rref.ref.Sentinel, Sentinel);
 				}
 			}
 
@@ -185,84 +237,54 @@ namespace xc{
 	public:
 		bool empty(){ return Size == 0; }
 		size_type size(){ return Size; }
-		T& front(){ return *(*(Sentinel.next)); }
-		T& back(){ return *(*(Sentinel.prev)); }
-		const T& front()const{ return *(*(Sentinel.next)); }
-		const T& back()const{ return *(*(Sentinel.prev)); }
-		void push_back(element& Elem){
-			element& Prev = *(Sentinel.prev);
-			element::connect(Prev, Elem);
-			element::connect(Elem, Sentinel);
-
+		T& front(){ return *static_cast<T*>(Sentinel.next); }
+		T& back(){ return *static_cast<T*>(Sentinel.prev); }
+		const T& front()const{ return *static_cast<const T*>(Sentinel.next); }
+		const T& back()const{ return *static_cast<const T*>(Sentinel.prev); }
+		void push_back(T& Elem){
 			++Size;
+			base::insert_element(Elem,  Sentinel);
 		}
 		void pop_back(){
 			if(empty())return;
 
-			element& Prev = *(Sentinel.prev);
-			element::connect(*(Prev.prev), Sentinel);
-			Prev.next = 0;
-			Prev.prev = 0;
-
 			--Size;
+			base::erase_element(*(Sentinel.prev));
 		}
-		void push_front(element& Elem){
-			element& Next = *(Sentinel.next);
-			element::connect(Sentinel, Elem);
-			element::connect(Elem, Next);
-
+		void push_front(T& Elem){
 			++Size;
+			base::insert_element(Elem, *(Sentinel.next));
 		}
 		void pop_front(){
 			if(empty())return;
 
-			element& Next = *(Sentinel.next);
-			element::connect(Sentinel, *(Next.next));
-			Next.next = 0;
-			Next.prev = 0;
-
 			--Size;
+			base::erase_element(*(Sentinel.next));
 		}
-		iterator insert(const_iterator pos, element& Elem){
-			element& Curr = *pos.current();
-			element::connect(*(Curr.prev), Elem);
-			element::connect(Elem, Curr);
-
+		iterator insert(const_iterator pos, T& Elem){
 			++Size;
+			base::insert_element(Elem, *const_cast<element*>(pos.current()));
 
-			return iterator(Curr.prev);
+			return iterator(Elem);
 		}
 		iterator erase(const_iterator pos){
 			if(pos == end())return end();
 
 			element& Curr = *pos.current();
 			element& Next = *(Curr.next);
-			element::connect(*(Curr.prev), *(Curr.next));
-			Curr.next = 0;
-			Curr.prev = 0;
 
 			--Size;
+			base::erase_element(Curr);
 
 			return iterator(&Next);
 		}
-		iterator erase(const_iterator pos, const_iterator last){
-			if(pos == end())return end();
+		iterator erase(const_iterator first, const_iterator last){
+			if(first == end())return end();
 
-			element& Prev = *(pos.current()->prev);
-
-			while(pos != last){
-				element& Curr = *(pos.current());
-				++pos;
-
-				Curr.next = 0;
-				Curr.prev = 0;
-				--Size;
-			}
-
-			element::connect(Prev, *(last.current()));
+			Size -= base::erase_range_element(*const_cast<element*>(first.current()), *const_cast<element*>(last.current()));
 			return iterator(last.current());
 		}
-		iterator find(const element& Elem){
+		iterator find(const T& Elem){
 			iterator Itr = begin();
 			for(;Itr != end(); ++Itr){
 				if(Itr.current() == &Elem)break;
@@ -279,17 +301,27 @@ namespace xc{
 		}
 		void splice(const_iterator pos, this_type& other){
 			if(other.empty())return;
-			auto AddSize = other.size();
 
-			element& Prev = *(pos.current().prev);
-			element& Curr = *(pos.current());
+			Size += other.size();
+			other.Size = 0;
 
-			element::connect(Prev, *(other.Sentinel.next));
-			element::connect(*(other.Sentinel.prev), Curr);
-			other.Sentinel.next = &(other.Sentinel);
-			other.Sentinel.prev = &(other.Sentinel);
+			base::splice_range_element(*(other.Sentinel.next), other.Sentinel, *const_cast<element*>(pos.current()));
+		}
+		void splice(const_iterator pos, this_type& other, const_iterator elem){
+			if(other.empty())return;
 
-			Size += AddSize;
+			++Size;
+			--(other.Size);
+
+			base::splice_element(*const_cast<element*>(elem.current()), *const_cast<element*>(pos.current()));
+		}
+		void splice(const_iterator pos, this_type& other, const_iterator first, const_iterator last){
+			int Num = std::distance(first, last);
+
+			Size += Num;
+			other.Size -= Num;
+
+			base::splice_range_element(*const_cast<element*>(first.current()), *const_cast<element*>(last.current()),*const_cast<element*>(pos.current()));
 		}
 	};
 	template<typename T>
@@ -297,12 +329,88 @@ namespace xc{
 		return xc::chain<T>::const_iterator(current());
 	}
 
+	struct sorted_chain_base;
+	struct sorted_chain_element{
+		friend struct sorted_chain_base;
+	private:
+		sorted_chain_element* next;
+	public:
+		sorted_chain_element() :next(0){}
+	};
+	struct sorted_chain_base{
+		typedef sorted_chain_element element;
+		static void clear_element(element& Elem){
+			Elem.next = 0;
+		}
+		static bool insert_after_element(element& New, element& Pos){
+			if(New.next != 0)return true;
+
+			New.next = Pos.next;
+			Pos.next = &New;
+
+			return false;
+		}
+		static bool splice_range_after_element(element& BeforeFirst, element& BeforeLast, element& Pos){
+			if(BeforeFirst.next != 0)return true;
+			if(BeforeLast.next != 0)return true;
+
+			element& Next = *(Pos.next);
+
+			Pos.next = BeforeFirst.next;
+			BeforeFirst.next = BeforeLast.next;
+			BeforeLast.next = &Next;
+
+			return false;
+		}
+		static bool splice_after_element(element& ElemBefore, element& Pos){
+			if(ElemBefore.next != 0)return true;
+
+			element& Elem = *(ElemBefore.next);
+			element& Next = *(Pos.next);
+
+			Pos.next = ElemBefore.next;
+			ElemBefore.next = Elem.next;
+			Elem.next = &Next;
+
+			return false;
+		}
+		static bool erase_after_element(element& Pos){
+			if(Pos.next == 0)return true;
+
+			element& Next = *(Pos.next);
+
+			Pos.next = Next.next;
+			clear_element(Next);
+
+			return false;
+		}
+		static unsigned int erase_range_after_element(element& BeforeFirst, element& BeforeLast){
+			if(BeforeFirst.next != 0)return true;
+			if(BeforeLast.next != 0)return true;
+
+			element* Ptr = BeforeFirst.next;
+			BeforeFirst.next = BeforeLast.next;
+
+			unsigned int Cnt = 0;
+			while(true){
+				element* Cur = Ptr;
+				Ptr = Ptr->next;
+				clear_element(*Cur);
+				++Cnt;
+				if(Cur == &BeforeLast)break;
+			}
+
+			return Cnt;
+		}
+	};
+
 	template<typename T, typename compare_ = std::less<T>>
 	struct sorted_chain{
 	public:
 		typedef chain<T> this_type;
 		typedef unsigned int size_type;
 		typedef compare_ compare;
+		typedef sorted_chain_base base;
 	public:
 		struct element{
 			friend struct sorted_chain<T, compare>;
@@ -411,37 +519,36 @@ namespace xc{
 		sorted_chain(){
 			Sentinel.next = &Sentinel;
 		}
-		sorted_chain(xc::rvalue_reference<this_type>& Otehr){
-			if(Otehr.ref.empty()){
+		sorted_chain(xc::rvalue_reference<this_type>& rref){
+			if(rref.ref.empty()){
+				Size = 0;
 				Sentinel.next = &Sentinel;
 				return;
 			}
 
-			element& Cur = Otehr.ref.Sentinel;
-			while(Cur.next != Otehr.ref.Sentinel){
-				Cur = *(Cur.next);
+			Size = rref.ref.Size;
+			rref.ref.Size = 0;
+			element* Ptr = rref.ref.Sentinel;
+			while(Ptr->next != &(rref.ref.Sentinel)){
+				Ptr = Ptr->next;
 			}
 
-			Sentinel.next = Otehr.ref.Sentinel.next;
-			Cur.next = &Sentinel.next;
-
-			Otehr.ref.Sentinel.next = &(Otehr.ref.Sentinel);
+			base::splice_range_after_element(rref.ref.Sentinel, Ptr, Sentinel);
 		}
-		this_type& operator=(xc::rvalue_reference<this_type>& Otehr){
-			if(this != &Otehr.ref){
+		this_type& operator=(xc::rvalue_reference<this_type>& rref){
+			if(this != &(rref.ref)){
 				clear();
 
-				if(Otehr.ref.empty())return;
+				if(!rref.ref.empty()){
+					Size = rref.ref.Size;
+					rref.ref.Size = 0;
+					element* Ptr = rref.ref.Sentinel;
+					while(Ptr->next != &(rref.ref.Sentinel)){
+						Ptr = Ptr->next;
+					}
 
-				element& Cur = Otehr.ref.Sentinel;
-				while(Cur.next != Otehr.ref.Sentinel){
-					Cur = *(Cur.next);
+					base::splice_range_after_element(rref.ref.Sentinel, Ptr, Sentinel);
 				}
-
-				Sentinel.next = Otehr.ref.Sentinel.next;
-				Cur.next = &Sentinel.next;
-
-				Otehr.ref.Sentinel.next = &(Otehr.ref.Sentinel);
 			}
 
 			return *this;
@@ -465,26 +572,22 @@ namespace xc{
 			element* p2 = &Sentinel;
 			while(true){
 				//check with p2 < Elem < p1?
-				if(p1 == &Sentinel || Comp(*Elem, *(*p1))){
-					element::connect(*p2, Elem);
-					element::connect(Elem, *p1);
+				if(p1 == &Sentinel || Comp(static_cast<T&>(Elem), static_cast<T&>(*p1))){
+					base::insert_after_element(Elem, *p1);
 					return;
 				}
 				p2 = p1->next;
 
 				//check with p1 < Elem < p2?
-				if(p2 == &Sentinel || Comp(*Elem, *(*p2))){
-					element::connect(*p1, Elem);
-					element::connect(Elem, *p2);
+				if(p2 == &Sentinel || Comp(static_cast<T&>(Elem), static_cast<T&>(*p2))){
+					base::insert_after_element(Elem, *p2);
 					return;
 				}
 				p1 = p2->next;
 			}
 		}
 		void pop(){
-			element& Next = *(Sentinel.next);
-			element::connect(Sentinel, *(Next.next));
-			Next.next = 0;
+			base::erase_after_element(Sentinel);
 		}
 		void swap(this_type& other){
 			std::swap(Sentinel.next, other.Sentinel.next);
